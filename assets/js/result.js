caeba8906a5bcfaa809e2d88a8a832a27beda8d5
Connor McGrath <connorjmcgrath93@gmail.com>
2022-10-31 18:37:03 -0700

    results page javascript

--- assets/js/result.js
+++ assets/js/result.js
@@ -10,19 +10,31 @@ var posterEl = document.getElementById('poster');
 
 var OMDBDataUrl = 'https://www.omdbapi.com/?apikey=767dc988&';
 var OMDBImageUrl = 'https://img.omdbapi.com/?apikey=767dc988&';
+var WikiURL = 'https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php';
 
 function getParams() {
     var searchParamsArr = document.location.search.split('&')
     var title = searchParamsArr[0].split('=').pop();
     var movieId = searchParamsArr[1].split('=').pop();
 
-    resultInfo(movieId);
-  }
+    resultInfo(movieId, title);
+}
 
-function resultInfo(query) {
-    var url = OMDBDataUrl + "i=" + query;
-    fetch(url)
+function resultInfo(queryID, queryTitle) {
+    var infoURL = OMDBDataUrl + "i=" + queryID + '&plot=full';
+    var titleAdapt = queryTitle.split('');
+
+    for (var i = 0; i < titleAdapt.length; i++) {
+        if(titleAdapt[i] === "+") {
+            titleAdapt.splice(i,1,"%20");
+        }
+    }
+    var wikiTitle = titleAdapt.join('');
+    console.log(wikiTitle);
+
+    fetch(infoURL)
         .then(function (response) {
+            console.log(response);
             if (!response.ok) {
                 throw response.json();
             }
@@ -31,7 +43,7 @@ function resultInfo(query) {
         })
         .then(function (info) {
             //assign info values to the various text and images in search-results.html
-            
+
             titleEl.textContent = info.Title;
             releaseEl.textContent += ": " + info.Released;
             synopsisEl.textContent = info.Plot;
@@ -42,8 +54,19 @@ function resultInfo(query) {
             act2El.textContent = actors[1];
             act3El.textContent = actors[2];
             posterEl.setAttribute("src", info.Poster);
-
-
         })
+
+    var wikiURL = WikiURL + '?action=parse&page=Pet_door&format=json';
+    console.log(wikiURL);
+    fetch(wikiURL)
+        .then(function (response) {
+            console.log(response);
+            if (!response.ok) {
+                throw response.json();
+            }
+
+            return response.json();
+        })
+    
 }
 getParams();
