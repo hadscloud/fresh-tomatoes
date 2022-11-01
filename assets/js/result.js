2204e1c0ad3689881185bbdecd185c46bf5c4fc9
Connor McGrath <connorjmcgrath93@gmail.com>
2022-10-31 19:26:53 -0700

    Fixed listing page results title

--- assets/js/result.js
+++ assets/js/result.js
@@ -1,4 +1,5 @@
 // Add more variables here as needed
+var searchBtnEl = document.querySelector('#search-btn');
 var titleEl = document.querySelector("#title");
 var releaseEl = document.querySelector("#release");
 var synopsisEl = document.querySelector("#synopsis");
@@ -20,21 +21,58 @@ function getParams() {
     resultInfo(movieId, title);
 }
 
+function handleSearchButton(event) {
+    event.preventDefault();
+  
+    var searchInputVal = document.querySelector('#search-input').value;
+    if (!searchInputVal) {
+      console.error('You need a search input value!');
+      return;
+    }
+  
+    var resultsQuery = OMDBDataUrl + 's=' + searchInputVal;
+    fetch(resultsQuery)
+      .then(function (response) {
+          if (!response.ok) {
+              throw response.json();
+          }
+  
+          return response.json();
+      })
+      .then(function (queryResults) {
+          var pages = Math.ceil(queryResults.totalResults/10);
+          var queryString = './search-listing.html?q=' + searchInputVal + '&totalpages=' + pages;
+          location.assign(queryString);
+      })
+}
+
+function getWikiInfo(title) {
+    var wikiURL = WikiURL + '?action=opensearch&search=' + title + '&format=json';
+    console.log(wikiURL);
+    fetch(wikiURL)
+        .then(function (response) {
+            console.log(response);
+            if (!response.ok) {
+                throw response.json();
+            }
+
+            return response.json();
+        })
+}
+
 function resultInfo(queryID, queryTitle) {
     var infoURL = OMDBDataUrl + "i=" + queryID + '&plot=full';
     var titleAdapt = queryTitle.split('');
 
     for (var i = 0; i < titleAdapt.length; i++) {
         if(titleAdapt[i] === "+") {
-            titleAdapt.splice(i,1,"%20");
+            titleAdapt.splice(i,1,"_");
         }
     }
     var wikiTitle = titleAdapt.join('');
-    console.log(wikiTitle);
 
     fetch(infoURL)
         .then(function (response) {
-            console.log(response);
             if (!response.ok) {
                 throw response.json();
             }
@@ -54,21 +92,17 @@ function resultInfo(queryID, queryTitle) {
             act2El.textContent = actors[1];
             act3El.textContent = actors[2];
             posterEl.setAttribute("src", info.Poster);
+            console.log(wikiTitle);
+            getWikiInfo(wikiTitle);
         })
 
-    var wikiURL = WikiURL + '?action=parse&page=Pet_door&format=json';
-    console.log(wikiURL);
-    fetch(wikiURL)
-        .then(function (response) {
-            console.log(response);
-            if (!response.ok) {
-                throw response.json();
-            }
-
-            return response.json();
-        })
-        .then(function (wiki) {
-            console.log(wiki);
-        }) 
+     
 }
+
 getParams();
+searchBtnEl.addEventListener('click', handleSearchButton);
+document.addEventListener('keypress', function(event) {
+    if(event.key === 'Enter') {
+        handleSearchButton(event);
+    }
+})
